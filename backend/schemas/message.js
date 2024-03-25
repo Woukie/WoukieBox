7e565936ac06d3fbafae6431a2c74de5d251123e
Woukie <lucaseldon@live.com>
2024-03-25 17:37:27 +0000

    Updated message schema

--- backend/schemas/message.js
+++ backend/schemas/message.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const Message = new Schema({
   parent_id: { type: Schema.Types.ObjectId },
+  child_id: { type: Schema.Types.ObjectId },
   sender_id: { type: Schema.Types.ObjectId, require: true },
   channel_id: { type: Schema.Types.ObjectId, require: true },
   sent_at: { type: Date, default: () => Date.now(), require: true },
