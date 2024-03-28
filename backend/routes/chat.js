64b5c23efe260af66b7fae64fa5bd561df3550ad
Woukie <lucaseldon@live.com>
2024-03-28 17:34:35 +0000

    Added a message box

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,36 +1,73 @@
+const Server = require("../schemas/server");
+const User = require("../schemas/user");
+const Channel = require("../schemas/channel");
+
 module.exports = function (io, jwt) {
   // Auth middleware
   io.use((socket, next) => {
-    console.log(socket.handshake);
     const token = socket.handshake.auth.token;
     if (!token) {
       return next(new Error("Authentication error"));
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
         console.log(err);
         return next(new Error("Invalid token"));
       }
 
-      socket.userId = decoded.userId; // Store user ID for authorization
+      const user = await User.findById(decoded.userId);
+
+      socket.user = user;
       next();
     });
   });
 
-  io.on("connection", (socket) => {
-    socket.on("join", (params) => {
-      console.log("Joined channel " + params.channel);
-      socket.join(params.channel);
-    });
+  io.on("connection", async (socket) => {
+    console.log(`${socket.user.username} connected`);
 
-    socket.on("sendMessage", (data) => {
-      console.log(data);
-      console.log("Sent message");
+    try {
+      const servers = await Server.find({
+        _id: { $in: socket.user.server_ids },
+      });
+
+      for (const server of servers) {
+        if (!server.user_ids.includes(socket.user._id)) {
+          continue;
+        }
+
+        const channels = await Channel.find({
+          _id: { $in: server.channel_ids },
+        });
+
+        for (const channel of channels) {
+          socket.join(channel._id);
+          console.log(`${socket.user.username} joined ${channel._id}`);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      // Idfk
+    }
+
+    socket.on("send_message", (data) => {
+      // TODO: Check if user has perms to send message to data.channel
+      io.to(data.channel).emit("on_message", {
+        message: data.message,
+        sender: socket.userId,
+      });
+
+      console.log(
+        socket.user.username +
+          " sent message '" +
+          data.message +
+          "' to " +
+          data.room
+      );
     });
 
     socket.on("disconnect", () => {
-      console.log("Disconnected");
+      console.log(socket.user.username + " disconnected");
     });
   });
 };
--- frontend/components/Chat.js
+++ frontend/components/Chat.js
@@ -1,14 +1,45 @@
 import { StyleSheet, View } from "react-native";
-import { ActivityIndicator, Text, useTheme } from "react-native-paper";
+import {
+  ActivityIndicator,
+  Surface,
+  Text,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
+import Textarea from "react-expanding-textarea";
 import { useWoukie } from "../contexts/WoukieContext";
 import { useAuth } from "../contexts/AuthenticationContext";
 import { useSocket } from "../contexts/SocketContext";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const placeholders = [
+  "ballse...",
+  "OoooohHH im so gay...",
+  "um hi...",
+  "AAAHHHHHH...",
+  "Message here...",
+  "my cock smells so bad...",
+  ":)...",
+  "GOD I hate n...",
+];
 
 export default function Chat() {
+  const [message, setMessage] = useState("");
+  const [placeholder, setPlaceHolder] = useState(
+    placeholders[Math.floor(Math.random() * placeholders.length)]
+  );
+
   const theme = useTheme();
-  const { connecting, connected, connect } = useSocket();
+  const { connected, sendMessage } = useSocket();
   const { selectedChannelID } = useWoukie();
+
+  useEffect(() => {
+    setPlaceHolder(
+      placeholders[Math.floor(Math.random() * placeholders.length)]
+    );
+  }, [selectedChannelID]);
+
+  const font = theme.fonts.bodyLarge;
 
   return (
     <View style={{ flex: 1 }}>
@@ -23,11 +54,42 @@ export default function Chat() {
           <View style={styles.root}>
             <ActivityIndicator size="large" />
             <Text style={{ padding: 16 }} variant="titleMedium">
-              Connecting to room: {selectedChannelID || ""}
+              Connecting...
             </Text>
           </View>
         )
       )}
+
+      <Surface style={{ padding: 0 }}>
+        <Textarea
+          maxLength={3000}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          placeholder={placeholder}
+          style={{
+            maxHeight: "300px",
+            display: "block",
+            padding: 14,
+            paddingLeft: 16,
+            paddingRight: 16,
+            fontFamily: font.fontFamily,
+            fontSize: font.fontSize,
+            fontStyle: font.fontStyle,
+            fontWeight: font.fontWeight,
+            letterSpacing: font.letterSpacing,
+            color: "rgb(230, 225, 229)",
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            outline: "none",
+            margin: 0,
+            appearance: "none",
+            borderRadius: 0,
+            boxSizing: "border-box",
+            resize: "none",
+            borderWidth: 0,
+          }}
+        />
+      </Surface>
     </View>
   );
 }
@@ -37,5 +99,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignSelf: "center",
+  },
+  container: {
+    flex: 1,
+  },
+  input: {
+    minHeight: 40,
+    maxHeight: 200,
+    margin: 20,
   },
 });
